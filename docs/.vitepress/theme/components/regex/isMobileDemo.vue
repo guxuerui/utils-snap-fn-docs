fea76e03b734f61ced7822cc56036ec1ac9e90a3
guxuerui <im.guxuerui@gmail.com>
2023-07-27 17:38:46 +0800

    feat(docs): add IsMobileDemo component to the theme

---
+++ docs/.vitepress/theme/components/regex/isMobileDemo.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { isMobile } from 'utils-snap-fn'
+
+const ifMobile = ref()
+
+onMounted(() => {
+  ifMobile.value = isMobile()
+})
+</script>
+
+<template>
+  <div>
+    Is this a mobile device?
+    <span class="text-emerald-600 text-xl ml-2">{{ ifMobile }}</span>
+  </div>
+</template>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -5,6 +5,7 @@ import 'uno.css'
 import Box from './components/base/Box.vue'
 import IsPhoneNumDemo from './components/regex/IsPhoneNumDemo.vue'
 import IsSafariDemo from './components/regex/isSafariDemo.vue'
+import IsMobileDemo from './components/regex/isMobileDemo.vue'
 
 export default {
   ...DefaultTheme,
@@ -12,5 +13,6 @@ export default {
     app.component('Box', Box)
     app.component('IsPhoneNumDemo', IsPhoneNumDemo)
     app.component('IsSafariDemo', IsSafariDemo)
+    app.component('IsMobileDemo', IsMobileDemo)
   },
 }
