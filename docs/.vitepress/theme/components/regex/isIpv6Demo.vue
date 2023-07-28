030a28584af5fa2dcdbd0ec161826cb33323ebbe
guxuerui <im.guxuerui@gmail.com>
2023-07-28 10:55:15 +0800

    feat(docs): add IsIpv6Demo component to the theme feat(docs/.vitepress/theme/components/regex/isIpv6Demo.vue): create IsIpv6Demo component with input field and validation logic for IPv6 addresses

---
+++ docs/.vitepress/theme/components/regex/isIpv6Demo.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { isIpv6 } from 'utils-snap-fn'
+
+const ifIpv6 = ref()
+
+const inputVal = ref('2001:0db8:85a3:0000:0000:8a2e:0370:7334')
+
+function reset() {
+  ifIpv6.value = isIpv6(inputVal.value)
+}
+
+onMounted(() => {
+  reset()
+})
+</script>
+
+<template>
+  <div>
+    <input
+      id="input"
+      v-model.trim="inputVal"
+      placeholder="Enter Ipv4..."
+      type="text"
+      autocomplete="true"
+      class="w-full h-10 pl-3 mb-3 text-left bg-gray-700 dark:bg-gray-800 focus:border focus:border-dashed focus:border-teal-500 rounded outline-none"
+      @keydown.enter="reset"
+    >
+    <div>
+      Is this a valid Id card number?
+      <span class="text-emerald-600 text-xl ml-2">{{ ifIpv6 }}</span>
+    </div>
+    <button class="btn mt-2" @click="reset">
+      TEST
+    </button>
+  </div>
+</template>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -9,6 +9,7 @@ import IsMobileDemo from './components/regex/isMobileDemo.vue'
 import IsEmailDemo from './components/regex/isEmailDemo.vue'
 import IsIdCardDemo from './components/regex/isIdCardDemo.vue'
 import IsIpv4Demo from './components/regex/isIpv4Demo.vue'
+import IsIpv6Demo from './components/regex/isIpv6Demo.vue'
 
 export default {
   ...DefaultTheme,
@@ -20,5 +21,6 @@ export default {
     app.component('IsEmailDemo', IsEmailDemo)
     app.component('IsIdCardDemo', IsIdCardDemo)
     app.component('IsIpv4Demo', IsIpv4Demo)
+    app.component('IsIpv6Demo', IsIpv6Demo)
   },
 }
